58580b4d43800343635dff72ca9dc8ae591bfbf0
ayushgupta <ayushgupta160803@gmail.com>
2024-08-10 03:43:33 -0700

    weathercompose

---
+++ app/src/main/java/com/example/weather_compose/network/weatherapiservice.kt
@@ -0,0 +1,31 @@
+package com.example.weather_compose.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+
+
+interface WeatherApiService {
+    @GET("weather")
+    suspend fun getWeather(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String,
+    ): WeatherResponse
+}
+
+object WeatherApi {
+    val retrofitService: WeatherApiService by lazy {
+        retrofit.create(WeatherApiService::class.java)
+    }
+}
